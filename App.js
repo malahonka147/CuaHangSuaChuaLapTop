47bf4b73acb80e0e7acb270b8a928b06d71b3a46
Nura <phantrong321@gmail.com>
2021-04-15 20:57:45 +0700

    .

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import ThemSP from './components/ThemSP';
 import SuaNV from './components/SuaNV';
 import ThemNV from './components/ThemNV';
 import CTPN from './components/CTPN';
+import ThemCTPN from './components/ThemCTPN';
 export default function App() {
   const Stack = createStackNavigator();
   return (
@@ -31,7 +32,8 @@ export default function App() {
         <Stack.Screen name="ThemPN" component={ThemPN} options={{headerShown: false,}} />
         <Stack.Screen name="SuaNV" component={SuaNV} options={{headerShown: false,}} />
         <Stack.Screen name="ThemNV" component={ThemNV} options={{headerShown: false,}} />
-        <Stack.Screen name="CTPN" component={ThemNV} options={{headerShown: false,}} />
+        <Stack.Screen name="CTPN" component={CTPN} options={{headerShown: false,}} />
+        <Stack.Screen name="ThemCTPN" component={ThemCTPN} options={{headerShown: false,}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
